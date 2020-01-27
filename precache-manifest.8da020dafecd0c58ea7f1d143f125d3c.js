288f6f5d7c6c42e8cf41af534d6ceaa9f6c3f845
Robert Bue <robert.bue@gmn.no>
2020-01-27 22:25:54 +0100

    Updates

---
+++ precache-manifest.8da020dafecd0c58ea7f1d143f125d3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c00c1d2bc4bfd1b38a5ea8681fb01b9",
+    "url": "/visage/index.html"
+  },
+  {
+    "revision": "ad4389e2d81945355857",
+    "url": "/visage/static/js/2.55430c7a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/visage/static/js/2.55430c7a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8e73ad25724f818e6e0",
+    "url": "/visage/static/js/main.63df26a3.chunk.js"
+  },
+  {
+    "revision": "cc11dc6ad2eccb6cf972",
+    "url": "/visage/static/js/runtime-main.3e6282ca.js"
+  }
+]);
