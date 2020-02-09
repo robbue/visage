73a2a8c2b7f91b2943e48a325b8d694bd1ab2105
Robert Bue <robert.bue@gmn.no>
2020-02-10 00:16:06 +0100

    Updates

---
+++ precache-manifest.898f84e397b431b38c960405a99e96c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1030c52357c9f6a38c1f5cdb9790d55",
+    "url": "/visage/index.html"
+  },
+  {
+    "revision": "cbe0cda02d495421c036",
+    "url": "/visage/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "cbe0cda02d495421c036",
+    "url": "/visage/static/js/2.b9b97ac8.chunk.js"
+  },
+  {
+    "revision": "f0d5791a33e59857ab6e1fdb81b7e66a",
+    "url": "/visage/static/js/2.b9b97ac8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a43bfcfc169111b6f463",
+    "url": "/visage/static/js/main.f529f7da.chunk.js"
+  },
+  {
+    "revision": "cc11dc6ad2eccb6cf972",
+    "url": "/visage/static/js/runtime-main.3e6282ca.js"
+  },
+  {
+    "revision": "94bea41899e8f40bea32160937e0fa4c",
+    "url": "/visage/static/media/webcam.94bea418.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/visage/precache-manifest.1117df1921c4ab7f2142293744ced8f6.js"
+  "/visage/precache-manifest.898f84e397b431b38c960405a99e96c1.js"
 );
 
 self.addEventListener('message', (event) => {
